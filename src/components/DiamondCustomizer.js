a78c4e0fe5c5d80c9d67e12fc0fc93d90be955b4
pcion <patrick.ciongoli@gmail.com>
2023-07-07 19:01:08 -0400

    added interactive diamond selector

--- src/components/DiamondCustomizer.js
+++ src/components/DiamondCustomizer.js
@@ -1,115 +1,204 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
+import "../styles/DiamondCustomizer.css";
+
+const initialFilterState = {
+   type: "",
+   minCarat: 0.2,
+   maxCarat: 5,
+   cut: "",
+   color: "",
+   clarity: "",
+   minPrice: 500,
+   maxPrice: 10000,
+};
+
+const filterReducer = (state, action) => {
+   switch (action.type) {
+      case "SET_FILTER":
+         return { ...state, [action.name]: action.value };
+      default:
+         return state;
+   }
+};
 
 const DiamondCustomizer = ({ onFilter }) => {
-   const [filters, setFilters] = useState({
-      type: "",
-      minCarat: "",
-      maxCarat: "",
-      cut: "",
-      color: "",
-      clarity: "",
-      minPrice: "",
-      maxPrice: "",
-   });
+   const [filters, dispatch] = useReducer(filterReducer, initialFilterState);
+   const [visibleFilter, setVisibleFilter] = useState(null);
 
-   const handleChange = (e) => {
-      setFilters({
-         ...filters,
-         [e.target.name]: e.target.value,
-      });
+   const cuts = ["Excellent", "Very Good", "Good", "Fair", "Poor"];
+   const colors = ["D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"];
+   const clarities = ["IF", "VVS1", "VVS2", "VS1", "VS2", "SI1", "SI2", "I1"];
+   const types = ["Round", "Princess"];
+
+   const handleChange = (filterName, value) => {
+      dispatch({ type: "SET_FILTER", name: filterName, value: value });
    };
 
-   const handleSubmit = () => {
-      onFilter(filters);
+   const toggleFilter = (filterName) => {
+      setVisibleFilter(visibleFilter === filterName ? null : filterName);
    };
 
    return (
-      <div>
+      <div className="diamond-customizer">
          <h3>Customize your Diamond</h3>
+         <div className="filters">
+            <div
+               className={`filter-btn ${
+                  visibleFilter === "type" ? "active" : ""
+               }`}
+               onClick={() => toggleFilter("type")}
+            >
+               Type: {filters.type || "Any"}
+               {visibleFilter === "type" && (
+                  <div className="filter-options">
+                     {types.map((type, index) => (
+                        <button
+                           key={index}
+                           onClick={() => handleChange("type", type)}
+                        >
+                           {type}
+                        </button>
+                     ))}
+                  </div>
+               )}
+            </div>
 
-         <label>
-            Shape:
-            <select name="type" value={filters.type} onChange={handleChange}>
-               <option value="">Any</option>
-               <option value="round">Round</option>
-               <option value="princess">Princess</option>
-               {/* Add other shapes here */}
-            </select>
-         </label>
+            <div
+               className={`filter-btn ${
+                  visibleFilter === "cut" ? "active" : ""
+               }`}
+               onClick={() => toggleFilter("cut")}
+            >
+               Cut: {filters.cut || "Any"}
+               {visibleFilter === "cut" && (
+                  <div className="filter-options">
+                     {cuts.map((cut, index) => (
+                        <button
+                           key={index}
+                           onClick={() => handleChange("cut", cut)}
+                        >
+                           {cut}
+                        </button>
+                     ))}
+                  </div>
+               )}
+            </div>
 
-         <label>
-            Min Carat:
-            <input
-               type="number"
-               name="minCarat"
-               value={filters.minCarat}
-               onChange={handleChange}
-            />
-         </label>
+            <div
+               className={`filter-btn ${
+                  visibleFilter === "color" ? "active" : ""
+               }`}
+               onClick={() => toggleFilter("color")}
+            >
+               Color: {filters.color || "Any"}
+               {visibleFilter === "color" && (
+                  <div className="filter-options">
+                     {colors.map((color, index) => (
+                        <button
+                           key={index}
+                           onClick={() => handleChange("color", color)}
+                        >
+                           {color}
+                        </button>
+                     ))}
+                  </div>
+               )}
+            </div>
 
-         <label>
-            Max Carat:
-            <input
-               type="number"
-               name="maxCarat"
-               value={filters.maxCarat}
-               onChange={handleChange}
-            />
-         </label>
+            <div
+               className={`filter-btn ${
+                  visibleFilter === "clarity" ? "active" : ""
+               }`}
+               onClick={() => toggleFilter("clarity")}
+            >
+               Clarity: {filters.clarity || "Any"}
+               {visibleFilter === "clarity" && (
+                  <div className="filter-options">
+                     {clarities.map((clarity, index) => (
+                        <button
+                           key={index}
+                           onClick={() => handleChange("clarity", clarity)}
+                        >
+                           {clarity}
+                        </button>
+                     ))}
+                  </div>
+               )}
+            </div>
 
-         <label>
-            Cut:
-            <select name="cut" value={filters.cut} onChange={handleChange}>
-               <option value="">Any</option>
-               <option value="Excellent">Excellent</option>
-               <option value="Very Good">Very Good</option>
-               <option value="Good">Good</option>
-               <option value="Fair">Fair</option>
-               <option value="Poor">Poor</option>
-            </select>
-         </label>
+            <div
+               className={`filter-btn ${
+                  visibleFilter === "carat" ? "active" : ""
+               }`}
+               onClick={() => toggleFilter("carat")}
+            >
+               Carat: {filters.minCarat} - {filters.maxCarat}
+               {visibleFilter === "carat" && (
+                  <div className="filter-options">
+                     <span>
+                        {filters.minCarat} - {filters.maxCarat}
+                     </span>
+                     <input
+                        type="range"
+                        min="0.2"
+                        max="5"
+                        step="0.1"
+                        value={filters.minCarat}
+                        onChange={(e) =>
+                           handleChange("minCarat", parseFloat(e.target.value))
+                        }
+                     />
+                     <input
+                        type="range"
+                        min="0.2"
+                        max="5"
+                        step="0.1"
+                        value={filters.maxCarat}
+                        onChange={(e) =>
+                           handleChange("maxCarat", parseFloat(e.target.value))
+                        }
+                     />
+                  </div>
+               )}
+            </div>
 
-         <label>
-            Color:
-            <input
-               type="text"
-               name="color"
-               value={filters.color}
-               onChange={handleChange}
-            />
-         </label>
-
-         <label>
-            Clarity:
-            <input
-               type="text"
-               name="clarity"
-               value={filters.clarity}
-               onChange={handleChange}
-            />
-         </label>
-
-         <label>
-            Min Price:
-            <input
-               type="number"
-               name="minPrice"
-               value={filters.minPrice}
-               onChange={handleChange}
-            />
-         </label>
-
-         <label>
-            Max Price:
-            <input
-               type="number"
-               name="maxPrice"
-               value={filters.maxPrice}
-               onChange={handleChange}
-            />
-         </label>
-
-         <button onClick={handleSubmit}>Find My Diamond</button>
+            <div
+               className={`filter-btn ${
+                  visibleFilter === "price" ? "active" : ""
+               }`}
+               onClick={() => toggleFilter("price")}
+            >
+               Price: ${filters.minPrice} - ${filters.maxPrice}
+               {visibleFilter === "price" && (
+                  <div className="filter-options">
+                     <span>
+                        ${filters.minPrice} - ${filters.maxPrice}
+                     </span>
+                     <input
+                        type="range"
+                        min="500"
+                        max="10000"
+                        step="100"
+                        value={filters.minPrice}
+                        onChange={(e) =>
+                           handleChange("minPrice", parseInt(e.target.value))
+                        }
+                     />
+                     <input
+                        type="range"
+                        min="500"
+                        max="10000"
+                        step="100"
+                        value={filters.maxPrice}
+                        onChange={(e) =>
+                           handleChange("maxPrice", parseInt(e.target.value))
+                        }
+                     />
+                  </div>
+               )}
+            </div>
+         </div>
+         <button onClick={() => onFilter(filters)}>Find My Diamond</button>
       </div>
    );
 };
